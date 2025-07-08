ee440bda534378d7eab34cd2c04ae2e328161e1f
rombetan123 <blockchaincun@gmail.com>
2025-07-08 15:49:33 -0500

    primera copia

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+@app.route("/")
+
+def home():
+    return "Hola bienvenidos campistas a mi aplicación"
+
+@app.route("/inicio/")
+
+def inicio():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
